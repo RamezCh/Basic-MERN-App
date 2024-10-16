c457bd2171a277406174d9d52efbd0e8578d6f76
RamezCh <ramiz_chreide@outlook.com>
2024-10-16 23:18:06 +0300

    Deploy Ready

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 
@@ -8,13 +9,20 @@ import productRoutes from './routes/product.route.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
 app.use(express.json()); // allows us to accept json data in the req.body
 
 app.use('/api/products', productRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Server is ready!');
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
